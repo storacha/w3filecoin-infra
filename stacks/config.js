89b30ede506ef246d95e0ef649a1d5f2f8e13420
Vasco Santos <santos.vasco10@gmail.com>
2023-01-12 16:48:19 +0100

    chore: add custom domain

---
+++ stacks/config.js
@@ -0,0 +1,17 @@
+/**
+ * Return the custom domain config for http api
+ * 
+ * @param {string} stage
+ * @param {string | undefined} hostedZone
+ * @returns {{domainName: string, hostedZone: string} | undefined}
+ */
+export function getCustomDomain (stage, hostedZone) {
+  // return no custom domain config if hostedZone not set
+  if (!hostedZone) {
+    return 
+  }
+  /** @type Record<string,string> */
+  const domainMap = { prod: hostedZone }
+  const domainName = domainMap[stage] ?? `${stage}.${hostedZone}`
+  return { domainName, hostedZone }
+}
