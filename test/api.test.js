8d99ccf6eae7dce074267eb3ce2ef7ee9de17a4d
Vasco Santos <santos.vasco10@gmail.com>
2023-07-06 15:54:55 +0200

    fix: upgrade sst

---
+++ test/api.test.js
@@ -0,0 +1,21 @@
+import { test } from './helpers/context.js'
+
+import git from 'git-rev-sync'
+
+import { getApiEndpoint, getStage } from './helpers/deployment.js'
+
+test.before(t => {
+  t.context = {
+    apiEndpoint: getApiEndpoint(),
+  }
+})
+
+test('GET /version', async t => {
+  const stage = getStage()
+  const response = await fetch(`${t.context.apiEndpoint}/version`)
+  t.is(response.status, 200)
+
+  const body = await response.json()
+  t.is(body.env, stage)
+  t.is(body.commit, git.long('.'))
+})
--- test/helpers/deployment.js
+++ test/helpers/deployment.js
@@ -1,42 +1,41 @@
-import {
-  State
-} from '@serverless-stack/core'
-import { DynamoDBClient } from '@aws-sdk/client-dynamodb'
 import { createRequire } from 'module'
+import fs from 'fs'
+import path from 'path'
 
-// Either seed.run deployment, or development deploy outputs-file
-// https://seed.run/docs/adding-a-post-deploy-phase.html#post-deploy-phase-environment
-export const stage = process.env.SEED_STAGE_NAME || State.getStage(process.cwd())
+export function getStage () {
+  const stage = process.env.SST_STAGE || process.env.SEED_STAGE_NAME
+  if (stage) {
+    return stage
+  }
+
+  const f = fs.readFileSync(path.join(
+    process.cwd(),
+    '.sst/stage'
+  ))
+
+  return f.toString()
+}
 
 export const getStackName = () => {
-  const require = createRequire(import.meta.url)
-  const sst = require('../../sst.json')
-  return `${stage}-${sst.name}`
+  const stage = getStage()
+  return `${stage}-w3filecoin`
 }
 
-export const getAwsRegion = () => {
+export const getApiEndpoint = () => {
+  const stage = getStage()
+
   // CI/CD deployment
   if (process.env.SEED_APP_NAME) {
-    return 'us-east-2'
+    return `https://${stage}.filecoin.web3.storage`
   }
 
-  return 'us-west-2'
-}
+  const require = createRequire(import.meta.url)
+  const testEnv = require(path.join(
+    process.cwd(),
+    '.sst/outputs.json'
+  ))
 
-/**
- * @param {string} tableName 
- */
-export const getDynamoDb = (tableName) => {
-  const region = getAwsRegion()
-  const endpoint = `https://dynamodb.${region}.amazonaws.com`
-
-  return {
-    client: new DynamoDBClient({
-      region,
-      endpoint
-    }),
-    tableName: `${getStackName()}-${tableName}`,
-    region,
-    endpoint
-  }
+  // Get Upload API endpoint
+  const id = 'ApiStack'
+  return testEnv[`${getStackName()}-${id}`].ApiEndpoint
 }
