d708a361fb0e8246144784abe6db5e8eff0d3215
Vasco Santos <santos.vasco10@gmail.com>
2023-07-17 13:33:11 +0200

    fix: simplify tests

--- packages/core/test/workflow/aggregator.test.js
+++ packages/core/test/workflow/aggregator.test.js
@@ -1,6 +1,6 @@
 import { testWorkflow as test } from '../helpers/context.js'
 
-import { Piece } from '@web3-storage/data-segment'
+import { Aggregate, Piece } from '@web3-storage/data-segment'
 
 import { createContentQueue } from '../../src/queue/content.js'
 import { createPieceQueue } from '../../src/queue/piece.js'
@@ -18,8 +18,10 @@ test.beforeEach(async t => {
 
 test('can consume empty piece queue', async t => {
   const { dbClient } = t.context
-  const pieceQueue = createPieceQueue(dbClient)
-  const aggregateQueue = createAggregateQueue(dbClient)
+  const {
+    pieceQueue,
+    aggregateQueue,
+  } = await prepare(dbClient)
 
   const { error, ok } = await aggregatorWorkflow.run({
     pieceQueue,
@@ -31,25 +33,13 @@ test('can consume empty piece queue', async t => {
 
 test('can consume piece queue with content to create aggregate', async t => {
   const { dbClient } = t.context
+  const {
+    contentQueue,
+    pieceQueue,
+    aggregateQueue,
+  } = await prepare(dbClient)
   const builderSize = Piece.PaddedSize.from(1 << 20)
-  const cargoSize = 8
-  const contentQueue = createContentQueue(dbClient)
-  const pieceQueue = createPieceQueue(dbClient)
-  const aggregateQueue = createAggregateQueue(dbClient)
-
-  // Prepare pieces by adding to content queue and then piece queue
-  const cargoItems = await getCargo(cargoSize)
-  const contentQueuePutResp = await Promise.all(
-    cargoItems.map(item => contentQueue.put(item.content))
-  )
-  t.falsy(contentQueuePutResp.find(resp => resp.error))
-  const pieceQueuePutResp = await Promise.all(cargoItems.map(item => pieceQueue.put({
-    link: item.piece.link,
-    // Write bumped size for testing purposes
-    size: item.piece.size,
-    content: item.content.link
-  })))
-  t.falsy(pieceQueuePutResp.find(resp => resp.error))
+  await addFixtures({ contentQueue, pieceQueue, builderSize })
 
   // Attempt to build aggregate
   const { error, ok } = await aggregatorWorkflow.run({
@@ -63,9 +53,82 @@ test('can consume piece queue with content to create aggregate', async t => {
   // Aggregate queue has entries to be offered
   const queuedAggregatesResponse = await aggregateQueue.peek()
   t.falsy(queuedAggregatesResponse.error)
-
   t.is(queuedAggregatesResponse.ok?.length, 1)
-  t.truthy(queuedAggregatesResponse.ok?.[0].link)
   t.truthy(queuedAggregatesResponse.ok?.[0].inserted)
+
+  // Validate aggregate
+  t.truthy(queuedAggregatesResponse.ok?.[0].link)
   t.deepEqual(queuedAggregatesResponse.ok?.[0].size, builderSize)
 })
+
+/**
+ * @param {import('kysely').Kysely<import('../../src/schema.js').Database>} dbClient
+ */
+async function prepare (dbClient) {
+  const contentQueue = createContentQueue(dbClient)
+  const pieceQueue = createPieceQueue(dbClient)
+  const aggregateQueue = createAggregateQueue(dbClient)
+
+  return {
+    contentQueue,
+    pieceQueue,
+    aggregateQueue,
+  }
+}
+
+/**
+ * @param {object} props
+ * @param {import('../../src/types').ContentQueue} props.contentQueue
+ * @param {import('../../src/types').PieceQueue} props.pieceQueue
+ * @param {import('@web3-storage/data-segment').PaddedPieceSize} props.builderSize
+ */
+async function addFixtures ({ contentQueue, pieceQueue, builderSize }) {
+  // Prepare pieces by adding to content queue, then piece queue and then aggregate queue
+  const cargoSize = 8
+  const cargoItems = (await getCargo(cargoSize))
+    .sort((a, b) => Number(a.piece.size - b.piece.size))
+  const contentQueuePutResp = await Promise.all(
+    cargoItems.map(item => contentQueue.put(item.content))
+  )
+  if (contentQueuePutResp.some(resp => resp.error)) {
+    throw new Error('could not write to content queue')
+  }
+
+  const pieceQueuePutResp = await Promise.all(cargoItems.map(item => pieceQueue.put({
+    link: item.piece.link,
+    // Write bumped size for testing purposes
+    size: item.piece.size,
+    content: item.content.link
+  })))
+  if (pieceQueuePutResp.some(resp => resp.error)) {
+    throw new Error('could not write to piece queue')
+  }
+
+  // Create aggregate
+  const builder = Aggregate.createBuilder({
+    size: builderSize
+  })
+  const addedPieces = []
+
+  for (const item of cargoItems) {
+    try {
+      builder.write({
+        root: item.piece.link.multihash.digest,
+        size: item.piece.size
+      })
+      addedPieces.push(item.piece.link)
+    } catch {}
+  }
+
+  const aggregateBuild = builder.build()
+  const aggregate = {
+    link: aggregateBuild.link(),
+    size: aggregateBuild.size,
+    pieces: addedPieces
+  }
+
+  return {
+    cargoItems,
+    aggregate
+  }
+}
